b25dd6f0e25a14b1d50b412a2ccdf77feee835fe
Guilherme Pejon <guilherme.pejon@housecallpro.com>
2026-01-02 21:08:14 -0300

    Added more chords

--- app/javascript/chord_data.js
+++ app/javascript/chord_data.js
@@ -11,11 +11,15 @@
 export const GUITAR_CHORDS = {
   // Major chords
   "C": { frets: [-1, 3, 2, 0, 1, 0], fingers: [0, 3, 2, 0, 1, 0] },
+  "C/G": { frets: [3, 3, 2, 0, 1, 0], fingers: [3, 4, 2, 0, 1, 0] },
   "D": { frets: [-1, -1, 0, 2, 3, 2], fingers: [0, 0, 0, 1, 3, 2] },
+  "D/F#": { frets: [2, 0, 0, 2, 3, 2], fingers: [1, 0, 0, 1, 3, 2] },
   "E": { frets: [0, 2, 2, 1, 0, 0], fingers: [0, 2, 3, 1, 0, 0] },
   "F": { frets: [1, 3, 3, 2, 1, 1], fingers: [1, 3, 4, 2, 1, 1], barreAt: 1 },
   "G": { frets: [3, 2, 0, 0, 0, 3], fingers: [2, 1, 0, 0, 0, 3] },
+  "G/B": { frets: [-1, 2, 0, 0, 0, 3], fingers: [0, 1, 0, 0, 0, 3] },
   "A": { frets: [-1, 0, 2, 2, 2, 0], fingers: [0, 0, 1, 2, 3, 0] },
+  "A/F#": { frets: [2, 2, 4, 4, 3, 2], fingers: [1, 1, 3, 4, 2, 1], barreAt: 2 },
   "B": { frets: [-1, 2, 4, 4, 4, 2], fingers: [0, 1, 2, 3, 4, 1], barreAt: 2, startFret: 2 },
   
   // Minor chords
@@ -49,6 +53,7 @@ export const GUITAR_CHORDS = {
   "Cm7": { frets: [-1, 3, 5, 3, 4, 3], fingers: [0, 1, 3, 1, 2, 1], barreAt: 3, startFret: 3 },
   "Dm7": { frets: [-1, -1, 0, 2, 1, 1], fingers: [0, 0, 0, 2, 1, 1] },
   "Em7": { frets: [0, 2, 0, 0, 0, 0], fingers: [0, 1, 0, 0, 0, 0] },
+  "Em7/B": { frets: [-1, 2, 0, 0, 0, 0], fingers: [0, 1, 0, 0, 0, 0] },
   "Fm7": { frets: [1, 3, 1, 1, 1, 1], fingers: [1, 3, 1, 1, 1, 1], barreAt: 1 },
   "Gm7": { frets: [3, 5, 3, 3, 3, 3], fingers: [1, 3, 1, 1, 1, 1], barreAt: 3, startFret: 3 },
   "Am7": { frets: [-1, 0, 2, 0, 1, 0], fingers: [0, 0, 2, 0, 1, 0] },
@@ -72,6 +77,7 @@ export const GUITAR_CHORDS = {
   "Cadd9": { frets: [-1, 3, 2, 0, 3, 0], fingers: [0, 2, 1, 0, 3, 0] },
   "Dadd9": { frets: [-1, -1, 0, 2, 3, 0], fingers: [0, 0, 0, 1, 2, 0] },
   "Eadd9": { frets: [0, 2, 2, 1, 0, 2], fingers: [0, 2, 3, 1, 0, 4] },
+  "Eadd2/B": { frets: [-1, 2, 2, 1, 0, 0], fingers: [0, 2, 3, 1, 0, 0] },
   "Fadd9": { frets: [1, 0, 3, 2, 1, 0], fingers: [1, 0, 4, 3, 2, 0] },
   "Gadd9": { frets: [3, 2, 0, 2, 0, 3], fingers: [2, 1, 0, 3, 0, 4] },
   "Aadd9": { frets: [-1, 0, 2, 2, 2, 0], fingers: [0, 0, 1, 2, 3, 0] },
@@ -657,10 +663,13 @@ export const UKULELE_CHORDS = {
   // Major chords
   "C": { frets: [0, 0, 0, 3], fingers: [0, 0, 0, 3] },
   "D": { frets: [2, 2, 2, 0], fingers: [1, 1, 1, 0] },
+  "D/F#": { frets: [2, 2, 2, 0], fingers: [1, 1, 1, 0] },
   "E": { frets: [1, 4, 0, 2], fingers: [1, 4, 0, 2] },
   "F": { frets: [2, 0, 1, 0], fingers: [2, 0, 1, 0] },
   "G": { frets: [0, 2, 3, 2], fingers: [0, 1, 3, 2] },
+  "G/B": { frets: [0, 2, 3, 2], fingers: [0, 1, 3, 2] },
   "A": { frets: [2, 1, 0, 0], fingers: [2, 1, 0, 0] },
+  "A/F#": { frets: [2, 1, 0, 0], fingers: [2, 1, 0, 0] },
   "B": { frets: [4, 3, 2, 2], fingers: [4, 3, 1, 1], barreAt: 2, startFret: 2 },
   
   // Minor chords
@@ -1647,10 +1656,13 @@ export const CAVAQUINHO_CHORDS = {
   // Major chords (based on standard Brazilian cavaquinho chord chart)
   "C": { frets: [0, 2, 3, 3], fingers: [0, 1, 2, 3] },
   "D": { frets: [2, 2, 4, 0], fingers: [1, 1, 3, 0] },
+  "D/F#": { frets: [2, 2, 4, 0], fingers: [1, 1, 3, 0] },
   "E": { frets: [1, 0, 2, 4], fingers: [1, 0, 2, 4] },
   "F": { frets: [3, 1, 2, 3], fingers: [3, 1, 2, 4] },
   "G": { frets: [0, 2, 0, 4], fingers: [0, 1, 0, 3] },
+  "G/B": { frets: [0, 2, 0, 4], fingers: [0, 1, 0, 3] },
   "A": { frets: [2, 2, 4, 2], fingers: [1, 1, 3, 1], barreAt: 2 },
+  "A/F#": { frets: [2, 2, 4, 2], fingers: [1, 1, 3, 1], barreAt: 2 },
   "B": { frets: [4, 4, 4, 4], fingers: [1, 1, 1, 1], barreAt: 4, startFret: 4 },
   
   // Minor chords
