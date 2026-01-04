701842adb543f8d4f82e5e7c2450e9f0cfd3e9cc
Guilherme Pejon <guilherme.pejon@housecallpro.com>
2026-01-03 21:57:21 -0300

    Improved UG scraper retries

--- app/jobs/ultimate_guitar/scrape_tab_job.rb
+++ app/jobs/ultimate_guitar/scrape_tab_job.rb
@@ -9,7 +9,7 @@ module UltimateGuitar
     queue_as :default
 
     retry_on UltimateGuitar::PageScraper::FetchError, wait: :polynomially_longer, attempts: 5
-    retry_on UltimateGuitar::PageScraper::ParseError, wait: 1.minute, attempts: 3
+    # ParseError retry is handled manually in perform method
     retry_on ActiveRecord::RecordInvalid, wait: 30.seconds, attempts: 2
 
     # Don't retry on duplicate key errors - the tab already exists
@@ -33,6 +33,15 @@ module UltimateGuitar
 
       # Be polite
       sleep(rand(1.0..2.0))
+    rescue UltimateGuitar::PageScraper::ParseError => e
+      # Don't retry if we can't locate the tab payload - this indicates a structural change in UG's page format
+      if e.message.include?("Could not locate tab payload within embedded page state")
+        Rails.logger.warn "[UG Scrape] Skipping tab due to page structure change: #{url} - #{e.message}"
+        nil # Don't retry, just skip
+      else
+        Rails.logger.error "[UG Scrape] Parse error for #{url}: #{e.message}"
+        raise
+      end
     rescue UltimateGuitar::PageScraper::SkippedError => e
       Rails.logger.info "[UG Scrape] #{e.message}"
       raise  # Let discard_on handle it
